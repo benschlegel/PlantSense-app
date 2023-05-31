36607290a40a0720ce134dbd768f2a22090400bb
Ben Schlegel <ben5.schlegel@gmail.com>
2023-05-31 22:16:40 +0200

    chore: add eslint, fix files

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -1,13 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import AnimatedLottieView from "lottie-react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-import AnimatedLottieView from 'lottie-react-native';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
 
 export default function TabThreeScreen() {
   return (
     <View style={styles.container}>
-      <AnimatedLottieView source={require('../../assets/lottie/Pothos.json')} autoPlay loop speed={0.9}></AnimatedLottieView>
+      <AnimatedLottieView
+        source={require("../../assets/lottie/Pothos.json")}
+        autoPlay
+        loop
+        speed={0.9}
+      />
     </View>
   );
 }
@@ -15,16 +20,7 @@ export default function TabThreeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
