b35e0a2be49ddf1c0cd476396c5b60829a7af810
Ben Schlegel <ben5.schlegel@gmail.com>
2023-06-03 22:59:21 +0200

    feat(LEDs): setting colors via app working

--- app/(notifications)/index.tsx
+++ app/(notifications)/index.tsx
@@ -1,13 +1,77 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import AnimatedLottieView from "lottie-react-native";
+import { useCallback } from "react";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
+import Colors from "../../constants/Colors";
+
+const baseServerUrl = "http://192.168.141.24";
+const ledEndpoint = "/led";
+
+async function sendLedRequest(red: number, green: number, blue: number) {
+  const payload = {
+    red: red,
+    green: green,
+    blue: blue,
+  };
+
+  // Send post request to esp
+  fetch(baseServerUrl + ledEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Success:", result);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+async function getBaseEndpoint() {
+  const response = await fetch(baseServerUrl);
+  const jsonData = await response.text();
+  console.log(jsonData);
+}
 
 export default function MainScreen() {
+  const sendHttpRequest = useCallback(async () => {
+    const response = await fetch(baseServerUrl);
+    const jsonData = await response.text();
+    console.log(jsonData);
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Notifications</Text>
+      <Text>Colors</Text>
+      <View style={styles.green}>
+        <TouchableOpacity
+          style={[styles.buttonColorContainer, styles.red]}
+          onPress={() => sendLedRequest(255, 0, 0)}
+        >
+          <Text>Red</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.green}>
+        <TouchableOpacity
+          style={styles.buttonColorContainer}
+          onPress={() => sendLedRequest(0, 255, 0)}
+        >
+          <Text>Green</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.green}>
+        <TouchableOpacity
+          style={[styles.buttonColorContainer, styles.blue]}
+          onPress={() => sendLedRequest(0, 0, 255)}
+        >
+          <Text>Blue</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -17,5 +81,27 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  green: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  buttonColorContainer: {
+    backgroundColor: Colors.light.primary2,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    paddingVertical: 12,
+    width: "40%",
+    borderColor: Colors.light.dark,
+    borderWidth: 1.5,
+    marginTop: 48,
+  },
+  red: {
+    backgroundColor: "#F45050",
+  },
+  blue: {
+    backgroundColor: "#0079FF",
   },
 });
---
+++ constants/Config.ts
@@ -0,0 +1,1 @@
+export const ServerAdresse = "http://localhost";
