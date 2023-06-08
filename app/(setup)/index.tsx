408d3370a9f3982bb8810fdc4ab689c2ec60ad65
Ben Schlegel <ben5.schlegel@gmail.com>
2023-06-08 21:45:45 +0200

    fix: fix setup screen

--- app/(setup)/index.tsx
+++ app/(setup)/index.tsx
@@ -8,15 +8,25 @@ import { getDeviceAvailable } from "../../helpers/functions";
 
 // How many times to attempt connection to esp for
 const connectionAttemps = 5;
+
+const defaultText = "Start scanning to get started";
+const deviceFoundText = "Device found! Click 'next' to configure it";
 export default function Setup() {
   const [isDeviceAvailable, setIsDeviceAvailable] = useState<
     undefined | boolean
   >(undefined);
 
+  const [isScanActive, setIsScanActive] = useState(false);
+
   useEffect(() => {
     //
   }, []);
   async function checkAvailable() {
+    setIsScanActive(true);
+    // Artifical delay for demo, remove
+    setTimeout(() => {
+      //
+    }, 600);
     let isAvailable = false;
     for (let i = 0; i < connectionAttemps; i++) {
       // Override isAvailable: true, if successful (and break)
@@ -26,19 +36,45 @@ export default function Setup() {
       }
     }
 
+    setIsScanActive(false);
+
     // set to isAvailable (true, if got response and false by default)
     setIsDeviceAvailable(isAvailable);
   }
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Connect to your device</Text>
+      <Text style={styles.subheaderText}>
+        {isDeviceAvailable ? deviceFoundText : defaultText}
+      </Text>
       <View style={styles.topContainer}>
-        <Text>Test</Text>
-        <Text>{"Device available: " + isDeviceAvailable}</Text>
+        {isDeviceAvailable && (
+          <View style={styles.deviceContainer}>
+            <View style={styles.plantContainer}>
+              <Text style={[styles.deviceText, { fontWeight: "bold" }]}>
+                PlantSense - Planty
+              </Text>
+            </View>
+          </View>
+        )}
+        {isScanActive && (
+          <View style={styles.deviceContainer}>
+            <View style={styles.plantContainer}>
+              <Text style={styles.deviceText}>Scanning...</Text>
+            </View>
+          </View>
+        )}
+      </View>
+      <View style={styles.deviceNotFoundContainer}>
+        <Text style={{ color: Colors.light.dark }}>{"Device not found? "}</Text>
+        <Text style={{ color: Colors.light.dark, fontWeight: "bold" }}>
+          click here
+        </Text>
       </View>
       <View style={styles.buttonContainer}>
         <StyledButton
           title="Scan for devices"
+          // buttonStyle={{ width: "100%" }}
           onPress={() => checkAvailable()}
         />
         <Link href="/(main)" asChild>
@@ -59,7 +95,7 @@ export default function Setup() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 30,
+    paddingHorizontal: 32,
     paddingVertical: 25,
     backgroundColor: Colors.light.background,
   },
@@ -67,6 +103,10 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 30,
     flexDirection: "column",
+    backgroundColor: Colors.light.primary2,
+    borderRadius: 14,
+    opacity: 0.8,
+    marginBottom: 20,
   },
   buttonContainer: {
     gap: 16,
@@ -84,5 +124,30 @@ const styles = StyleSheet.create({
     color: Colors.light.dark,
     fontSize: 26,
     opacity: 0.8,
+  },
+  deviceContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 26,
+  },
+  plantContainer: {
+    backgroundColor: Colors.light.light,
+    height: 46,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  deviceText: {
+    color: Colors.light.dark,
+  },
+  subheaderText: {
+    color: Colors.light.dark,
+    fontSize: 15,
+  },
+  deviceNotFoundContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: -12,
+    marginBottom: 20,
+    marginLeft: 10,
   },
 });
