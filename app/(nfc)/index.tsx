727d746d64169c6a5737b4ce9d6742ba7acae33f
Ben Schlegel <ben5.schlegel@gmail.com>
2023-06-15 09:02:39 +0200

    style: create nfc screen, add wifi json

--- app/(nfc)/index.tsx
+++ app/(nfc)/index.tsx
@@ -1,14 +1,59 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import AnimatedLottieView from "lottie-react-native";
 
-import HappyPlanty from "../../assets/lottie/nfc.json";
+import NfcScan from "../../assets/lottie/nfc.json";
+import Wifi from "../../assets/lottie/wifi.json";
 import Colors from "../../constants/Colors";
+import { MonoText } from "../../components/StyledText";
+import StyledButton from "../../components/StyledButton";
 
-export default function index() {
+const defaultText =
+  "Scan your PlantSense chip and connect to it's wifi to get started.";
+const deviceFoundText =
+  "Device has been found! Please proceed to set up your new device.";
+
+const defaultButtonText = "Connect to device";
+export default function NfcScreen() {
+  const [deviceFound, setDeviceFound] = useState(false);
+  const [isScanning, setIsScanning] = useState(false);
+
+  async function scanForDevice() {
+    setIsScanning(true);
+    setTimeout(() => {
+      setIsScanning(false);
+    }, 500);
+  }
   return (
     <View style={styles.container}>
-      <AnimatedLottieView source={HappyPlanty} autoPlay loop speed={0.95} />
+      <View style={styles.headerContainer}>
+        <MonoText style={styles.headerText}>PlantSense</MonoText>
+      </View>
+      <View style={styles.subheaderContainer}>
+        <Text style={styles.subheaderText}>
+          {deviceFound ? deviceFoundText : defaultText}
+        </Text>
+      </View>
+      <AnimatedLottieView
+        source={NfcScan}
+        autoPlay
+        loop
+        speed={0.95}
+        style={styles.lottie}
+      />
+      <View style={styles.buttonContainer}>
+        <StyledButton
+          title="Connect to device"
+          buttonStyle={{
+            borderWidth: 0,
+            paddingHorizontal: 16,
+            paddingVertical: 14,
+            width: "60%",
+          }}
+          isLoading={isScanning}
+          onPress={() => scanForDevice()}
+        />
+      </View>
     </View>
   );
 }
@@ -17,5 +62,37 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.light.background,
     flex: 1,
+    paddingHorizontal: 32,
+  },
+  headerContainer: {
+    marginTop: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 32,
+    color: Colors.light.dark,
+    fontWeight: "bold",
+  },
+  lottie: {
+    height: 10,
+    // width: "80%",
+    flex: 1,
+    justifyContent: "center",
+    alignSelf: "center",
+    // backgroundColor: "red",
+  },
+  buttonContainer: {
+    // flex: 1,
+    marginBottom: 100,
+    marginTop: 20,
+  },
+  subheaderContainer: {
+    marginTop: 20,
+    marginBottom: 30,
+  },
+  subheaderText: {
+    color: Colors.light.dark,
+    fontSize: 16,
   },
 });
